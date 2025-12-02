dd86d046f19901076c4e8f7b56b96173a5c828da
Euluna Gotami <eulunag@gmail.com>
2025-12-02 00:31:53 -0800

    ??

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
+// src/main.tsx
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import { HashRouter } from 'react-router-dom'
+// FIX: Change BrowserRouter to HashRouter for GitHub Pages compatibility
+import { HashRouter } from 'react-router-dom' 
 import App from './App'
 import { CartProvider } from './contexts/CartContext'
 import './index.css'
@@ -8,7 +10,7 @@ import './index.css'
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CartProvider>
-      <HashRouter>
+      <HashRouter> {/* FIX: Use HashRouter here */}
         <App />
       </HashRouter>
     </CartProvider>
